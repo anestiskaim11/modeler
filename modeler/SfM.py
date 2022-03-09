3ac41965cdafd7ed1d492fb75eea36943bbf2eb8
anestiskaim11 <81708900+anestiskaim11@users.noreply.github.com>
2022-03-09 21:08:01 +0200

    given matching points example

--- modeler/SfM.py
+++ modeler/SfM.py
@@ -156,87 +156,93 @@ class SfM:
 			print('Using ' + str(image_name1) + ' and ' + str(image_name2))
 
 			if self.debug_mode:
-				plt.subplot('121')
+				plt.subplot(121)
 				plt.imshow(frame1)
 
-				plt.subplot('122')
+				plt.subplot(122)
 				plt.imshow(frame2)
 				plt.show()
 
 			print('Matching...')
 
-			robust_matcher = ImagePair(frame1, frame2)
+			'''robust_matcher = ImagePair(frame1, frame2)
 			kp1 = robust_matcher.get_keypoints_image1()
 			kp2 = robust_matcher.get_keypoints_image2()
-			colors = robust_matcher.get_colors(frame1)
+			colors = robust_matcher.get_colors(frame1)'''
 
-			if robust_matcher.has_enough_matches():
-				robust_matcher.display_and_save_matches_image()
-				print('Enough Matches!')
 
-				K = self.triangulation.find_matrix_K(frame1)
-				if initial_3d_model:
-					print('Calculating initial camera matrices...')
-					p1, p2 = self.triangulation.find_camera_matrices(robust_matcher.get_fundamental_matrix())
+			#robust_matcher.display_and_save_matches_image()
+			print('Enough Matches!')
+			kp1, kp2 = self.find_kp()
+			K = self.triangulation.find_matrix_K(frame1)
+			if initial_3d_model:
+				print('Calculating initial camera matrices...')
+				points1 = []
+				points2 = []
+				for point1, point2 in zip(kp1, kp2):
+					points1.append(point1.pt)
+					points2.append(point2.pt)
 
-					print('Creating initial 3D model...')
-					point_cloud = self.triangulation.triangulate(kp1, kp2, K, p1, p2, point_cloud)
-					self.current.add_all_entries(kp2, point_cloud)
+				if len(points1) > 0 and len(points2) > 0:
+					fundamental, _ = cv.findFundamentalMat(np.float32(points1), np.float32(points2), method=cv.FM_RANSAC, ransacReprojThreshold=5, confidence=0.9)
+				p1, p2 = self.triangulation.find_camera_matrices(fundamental)
 
-					if self.debug_mode:
-						print('Initial lookup table size is: ' + str(self.current.table_size()))
-					initial_3d_model = False
-				else:
-					self.prev.init()
-					# TODO one might have to call .copy() on current to eliminate unwanted behaviours
-					self.prev = self.current.copy()
+				print('Creating initial 3D model...')
+				point_cloud = self.triangulation.triangulate(kp1, kp2, K, p1, p2, point_cloud)
+				self.current.add_all_entries(kp2, point_cloud)
 
-					if self.current == self.table2:
-						self.current = self.table1.copy()
-					elif self.current == self.table1:
-						self.current = self.table2.copy()
+				if self.debug_mode:
+					print('Initial lookup table size is: ' + str(self.current.table_size()))
+				initial_3d_model = False
+			else:
+				self.prev.init()
+				# TODO one might have to call .copy() on current to eliminate unwanted behaviours
+				self.prev = self.current.copy()
 
-					if self.debug_mode:
-						print('LookupTable size is: ' + str(self.prev.table_size()))
-						print('New Table size is: ' + str(self.current.table_size()))
+				if self.current == self.table2:
+					self.current = self.table1.copy()
+				elif self.current == self.table1:
+					self.current = self.table2.copy()
 
-					p1 = p2.copy()
-					p2 = SfM.find_second_camera_matrix(p2, kp2, kp1, self.current, self.prev, K)
+				if self.debug_mode:
+					print('LookupTable size is: ' + str(self.prev.table_size()))
+					print('New Table size is: ' + str(self.current.table_size()))
 
-					if self.debug_mode:
-						print('New table size after adding known 3D points: ' + str(self.current.table_size()))
+				p1 = p2.copy()
+				p2 = SfM.find_second_camera_matrix(p2, kp2, kp1, self.current, self.prev, K)
 
-					print('Triangulating...')
-					point_cloud = self.triangulation.triangulate(kp1, kp2, K, p1, p2, point_cloud)
-					self.current.add_all_entries(kp2, point_cloud)
+				if self.debug_mode:
+					print('New table size after adding known 3D points: ' + str(self.current.table_size()))
 
-				number_of_points_added = len(kp1)
+			print('Triangulating...')
+			point_cloud = self.triangulation.triangulate(kp1, kp2, K, p1, p2, point_cloud)
+			self.current.add_all_entries(kp2, point_cloud)
 
-				print('Start writing points to file...')
-				self.ply.insert_header(len(point_cloud), file_number)
+			number_of_points_added = len(kp1)
 
-				for i in range(prev_number_of_points_added):
-					point = point_cloud[i]
-					blue = point_cloud[i].b
-					red = point_cloud[i].r
-					green = point_cloud[i].g
-					self.ply.insert_point(point.x, point.y, point.z, red, green, blue, file_number)
+			print('Start writing points to file...')
+			self.ply.insert_header(len(point_cloud), file_number)
 
-				for i in range(number_of_points_added):
-					point = point_cloud[i + prev_number_of_points_added]
-					point_color = colors[i]
-					point_cloud[i + prev_number_of_points_added].b = point_color[0]
-					point_cloud[i + prev_number_of_points_added].g = point_color[1]
-					point_cloud[i + prev_number_of_points_added].r = point_color[2]
+			for i in range(prev_number_of_points_added):
+				point = point_cloud[i]
+				blue = point_cloud[i].b
+				red = point_cloud[i].r
+				green = point_cloud[i].g
+				self.ply.insert_point(point.x, point.y, point.z, red, green, blue, file_number)
 
-					self.ply.insert_point(point.x, point.y, point.z, point_color[0], point_color[1], point_color[2],
+			for i in range(number_of_points_added):
+				point = point_cloud[i + prev_number_of_points_added]
+				#point_color = colors[i]
+				point_cloud[i + prev_number_of_points_added].b = 255
+				point_cloud[i + prev_number_of_points_added].g = 255
+				point_cloud[i + prev_number_of_points_added].r = 255
+
+				self.ply.insert_point(point.x, point.y, point.z, 255, 255, 255,
 												 file_number)
 
-				file_number += 1
-				prev_number_of_points_added = number_of_points_added + prev_number_of_points_added
+			file_number += 1
+			prev_number_of_points_added = number_of_points_added + prev_number_of_points_added
 
-			else:
-				print('Not enough matches')
 
 			picture_number1 = picture_number2 % self.number_of_images
 			picture_number2 = (picture_number2 + factor) % self.number_of_images
@@ -245,7 +251,8 @@ class SfM:
 			if count % self.number_of_images == self.number_of_images - 1:
 				picture_number2 += 1
 				factor += 1
-				
+			break;
+
 			image_name1 = self.images_dir + 'im' + str(picture_number1) + '.jpg'
 			image_name2 = self.images_dir + 'im' + str(picture_number2) + '.jpg'
 			frame1 = cv.imread(image_name1)
@@ -255,3 +262,75 @@ class SfM:
 
 		print('Done')
 
+	def find_kp(self):
+		kp1 = []
+		kp2 = []
+		vertical_sampling_rate = 50
+		horizontal_sampling_rate = 20
+		top_left = [[230, 106], [112, 161]]
+		bottom_left = [[219, 234], [96, 282]]
+		top_right = [[261, 106], [136, 162]]
+		bottom_right = [[264, 233], [130, 282]]
+		top_right_side = [[265, 106], [149, 166]]
+		bottom_right_side = [[270, 236], [148, 285]]
+		ratio = [(top_left[0][0] - bottom_left[0][0]) * 1.0 / (top_left[1][0] - bottom_left[1][0]),
+				 (top_right[0][1] - top_left[0][1]) * 1.0 / (top_right[1][1] - top_left[1][1])]
+
+		kp1.append(cv.KeyPoint(x=top_left[0][0], y=top_left[0][1], size=1))
+		kp2.append(cv.KeyPoint(x=top_left[1][0], y=top_left[1][1], size=1))
+
+		kp1.append(cv.KeyPoint(x=top_right[0][0], y=top_right[0][1], size=1))
+		kp2.append(cv.KeyPoint(x=top_right[1][0], y=top_right[1][1], size=1))
+
+		kp1.append(cv.KeyPoint(x=bottom_left[0][0], y=bottom_left[0][1], size=1))
+		kp2.append(cv.KeyPoint(x=bottom_left[1][0], y=bottom_left[1][1], size=1))
+
+		kp1.append(cv.KeyPoint(x=bottom_right[0][0], y=bottom_right[0][1], size=1))
+		kp2.append(cv.KeyPoint(x=bottom_right[1][0], y=bottom_right[1][1], size=1))
+
+		kp1.append(cv.KeyPoint(x=bottom_right_side[0][0], y=bottom_right_side[0][1], size=1))
+		kp2.append(cv.KeyPoint(x=bottom_right_side[1][0], y=bottom_right_side[1][1], size=1))
+
+		kp1.append(cv.KeyPoint(x=top_right_side[0][0], y=top_right_side[0][1], size=1))
+		kp2.append(cv.KeyPoint(x=top_right_side[1][0], y=top_right_side[1][1], size=1))
+
+		stepx1 = -(top_left[0][0] - bottom_left[0][0])*1.0/vertical_sampling_rate
+		stepy1 = -(top_left[0][1] - bottom_left[0][1])*1.0/vertical_sampling_rate
+		stepx2 = -(top_left[1][0] - bottom_left[1][0])*1.0/vertical_sampling_rate
+		stepy2 = -(top_left[1][1] - bottom_left[1][1])*1.0/vertical_sampling_rate
+		for i in range(vertical_sampling_rate):
+			kp1.append(cv.KeyPoint(x=top_left[0][0]+i*stepx1, y=top_left[0][1]+i*stepy1, size=1))
+			kp2.append(cv.KeyPoint(x=top_left[1][0]+i*stepx2, y=top_left[1][1]+i*stepy2, size=1))
+
+		stepx1 = -(top_right[0][0] - bottom_right[0][0]) * 1.0 / vertical_sampling_rate
+		stepy1 = -(top_right[0][1] - bottom_right[0][1]) * 1.0 / vertical_sampling_rate
+		stepx2 = -(top_right[1][0] - bottom_right[1][0]) * 1.0 / vertical_sampling_rate
+		stepy2 = -(top_right[1][1] - bottom_right[1][1]) * 1.0 / vertical_sampling_rate
+		for i in range(vertical_sampling_rate):
+			kp1.append(cv.KeyPoint(x=top_right[0][0] + i * stepx1, y=top_right[0][1] + i * stepy1, size=1))
+			kp2.append(cv.KeyPoint(x=top_right[1][0] + i * stepx2, y=top_right[1][1] + i * stepy2, size=1))
+
+		stepx1 = -(top_right_side[0][0] - bottom_right_side[0][0]) * 1.0 / vertical_sampling_rate
+		stepy1 = -(top_right_side[0][1] - bottom_right_side[0][1]) * 1.0 / vertical_sampling_rate
+		stepx2 = -(top_right_side[1][0] - bottom_right_side[1][0]) * 1.0 / vertical_sampling_rate
+		stepy2 = -(top_right_side[1][1] - bottom_right_side[1][1]) * 1.0 / vertical_sampling_rate
+		for i in range(vertical_sampling_rate):
+			kp1.append(cv.KeyPoint(x=top_right_side[0][0] + i * stepx1, y=top_right_side[0][1] + i * stepy1, size=1))
+			kp2.append(cv.KeyPoint(x=top_right_side[1][0] + i * stepx2, y=top_right_side[1][1] + i * stepy2, size=1))
+
+		stepx1 = -(top_left[0][0] - top_right[0][0]) * 1.0 / horizontal_sampling_rate
+		stepy1 = -(top_left[0][1] - top_right[0][1]) * 1.0 / horizontal_sampling_rate
+		stepx2 = -(top_left[1][0] - top_right[1][0]) * 1.0 / horizontal_sampling_rate
+		stepy2 = -(top_left[1][1] - top_right[1][1]) * 1.0 / horizontal_sampling_rate
+		for i in range(horizontal_sampling_rate):
+			kp1.append(cv.KeyPoint(x=top_left[0][0] + i * stepx1, y=top_left[0][1] + i * stepy1, size=1))
+			kp2.append(cv.KeyPoint(x=top_left[1][0] + i * stepx2, y=top_left[1][1] + i * stepy2, size=1))
+
+		stepx1 = -(top_right[0][0] - top_right_side[0][0]) * 1.0 / horizontal_sampling_rate
+		stepy1 = -(top_right[0][1] - top_right_side[0][1]) * 1.0 / horizontal_sampling_rate
+		stepx2 = -(top_right[1][0] - top_right_side[1][0]) * 1.0 / horizontal_sampling_rate
+		stepy2 = -(top_right[1][1] - top_right_side[1][1]) * 1.0 / horizontal_sampling_rate
+		for i in range(horizontal_sampling_rate):
+			kp1.append(cv.KeyPoint(x=top_right[0][0] + i * stepx1, y=top_right[0][1] + i * stepy1, size=1))
+			kp2.append(cv.KeyPoint(x=top_right[1][0] + i * stepx2, y=top_right[1][1] + i * stepy2, size=1))
+		return kp1, kp2
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 from modeler import SfM
 
 
-sfm = SfM('results/', True, 'IMG_6538.MOV', 30, debug_mode=False)
+sfm = SfM('results/', True, 'IMG_6538.MOV', 30, debug_mode=True)
 print('Done')
 sfm.find_structure_from_motion()
